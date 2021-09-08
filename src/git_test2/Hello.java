18feda1cb38f2c85c7f0d528fe5b126116a7177c
Lee, Jaehyeok <dlwogur15@gmail.com>
2021-09-08 22:00:01 +0900

    커밋과 푸시 확인입니다.

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -3,7 +3,7 @@ package git_test2;
 public class Hello {
 
 	public void say() {
-		System.out.println("안녕하세요!");
+		System.out.println("안녕하세요! 커밋 확인입니다!");
 	}
 
 	public static void main(String[] args) {
