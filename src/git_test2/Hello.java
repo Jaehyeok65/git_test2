25d1ce98636a1759d453469203ee201df68ad871
Lee, Jaehyeok <dlwogur15@gmail.com>
2021-09-08 21:34:41 +0900

    최초 커밋

---
+++ src/git_test2/Hello.java
@@ -0,0 +1,14 @@
+package git_test2;
+
+public class Hello {
+
+	public void say() {
+		System.out.println("hello! world!");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/git_test2/Main.java
@@ -0,0 +1,12 @@
+package git_test2;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		new Hello().say();
+		// TODO Auto-generated method stub
+
+	}
+
+}
