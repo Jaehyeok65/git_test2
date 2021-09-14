5d67cfd368bc03107e3a27e173c5768a8444634c
Lee, Jaehyeok <dlwogur15@gmail.com>
2021-09-14 18:25:23 +0900

    화면 출력 방법 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -2,10 +2,10 @@ package git_test2;
 
 public class Hello {
 	
-	String message = "안녕하세요!"
+	String message = "안녕하세요!";
 
-	public void say() {
-		System.out.println(message);
+	public String say() {
+		return message;
 	}
 
 	public static void main(String[] args) {
