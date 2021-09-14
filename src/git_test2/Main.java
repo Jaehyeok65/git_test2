854edf9c9fc0e39b1e1ba60e9bb5a68ae19e7460
Lee, Jaehyeok <dlwogur15@gmail.com>
2021-09-14 17:58:01 +0900

    Main.java 수정

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Hello hi = new Hello();
-		hi.say();
+		Hello hello = new Hello();
+		hello.say();
 		// TODO Auto-generated method stub
 
 	}
